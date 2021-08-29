da1573ec8f18973d6ebf864e93004a702a2dc073
Anthony Chan <anthony.chan@gogotech.hk>
2021-08-29 22:18:20 +0800

    init saga

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,3 +1,11 @@
+import ReducerActions from '../actions'
+import { takeLatest } from 'redux-saga/effects'
+
+
+function* fetchTransactions() {
+  console.log('fetchTransactions')
+}
+
 export function* helloSaga() {
-  yield console.log("Hello Sagas!");
+  yield takeLatest(ReducerActions.GET_TRANSACTION, fetchTransactions)
 }
